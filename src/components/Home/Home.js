12c598dc58b8c06b4ecc92fd6db804bc08ac5320
Bernard Historillo <bernardhistorillo1@gmail.com>
2023-12-05 01:58:09 +0800

    Add featured artist and process sections

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import icon from '../../img/logo.png';
 import bg1 from '../../img/home/bg-1.webp';
 import avatar from '../../img/home/avatar.webp';
+import rocket from '../../img/home/rocket.webp';
+import marso from '../../img/home/marso.webp';
 
 function Home(props) {
     useEffect(function() {
@@ -13,7 +15,7 @@ function Home(props) {
     return (
         <div className="home bg-white">
             {/* Navbar*/}
-            <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white py-2">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white py-2 tw-border-b-[#000000] tw-border-b-[1px]">
                 <div className="container">
                     <a className="navbar-brand d-flex align-items-center" href="https://creativelegazpi.ph">
                         <img src={icon} className="tw-h-[53px]" alt="Creative Legazpi" />
@@ -72,11 +74,101 @@ function Home(props) {
                             <p className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700">Avatar: The Way of Water</p>
                             <p className="font-size-120 font-size-lg-130 font-size-xl-150 font-weight-700 mb-4">by Raf Banzuela</p>
 
-                            <p className="mb-5 pb-lg-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                            <p className="font-size-110 mb-5 pb-lg-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
 
                             <div className="">
                                 <button className="btn btn-custom-1 tw-rounded-[30px] px-5 tw-pb-[10px]">Learn more</button>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-color-1 py-5">
+                <div className="container py-5">
+                    <div className="text-center mb-5 mt-3">
+                        <img src={rocket} className="tw-w-[220px] d-inline-block" alt="Creative Legazpi" />
+                    </div>
+
+                    <div className="row justify-content-center mb-5">
+                        <div className="col-sm-11 col-md-10 col-lg-9 col-xl-8 col-xxl-7">
+                            <div className="text-center">
+                                <p className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700 mb-4">The Legazpi Artist Launchpad</p>
+                                <p className="font-size-110 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="text-center">
+                        <button className="btn btn-custom-1 tw-rounded-[30px] px-5 tw-pb-[10px]">Join the community</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row align-items-center mx-0">
+                <div className="col-md-6 col-lg-7 order-1 order-md-0">
+                    <div className="d-flex justify-content-end w-100 py-5">
+                        <div className="md:tw-max-w-[calc((720px/12)*6)] lg:tw-max-w-[calc((960px/12)*7)] xl:tw-max-w-[calc((1140px/12)*7)] xxl:tw-max-w-[calc((1320px/12)*7)] py-5 py-md-0 py-lg-5">
+                            <div className="text-center text-md-start pe-md-3 pe-lg-5">
+                                <p className="font-size-120 font-size-lg-130 font-size-xl-150 font-weight-500 mb-3">Featured Collection</p>
+                                <p className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700">Marso</p>
+                                <p className="font-size-120 font-size-lg-130 font-size-xl-150 font-weight-700 mb-4">“Glad to work with a group of intellectuals who aspire to be one of the best in the nft space.”</p>
+
+                                <p className="font-size-110 mb-5 pb-lg-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                                <div className="">
+                                    <button className="btn btn-custom-2 tw-rounded-[30px] px-5 tw-pb-[10px] me-sm-1 me-md-0 me-lg-1 tw-w-[217px] mb-2 mb-sm-0 mb-md-2 mb-lg-0">View Collections</button>
+                                    <button className="btn btn-custom-3 tw-rounded-[30px] px-5 tw-pb-[10px] me-sm-1 ms-md-0 ms-lg-1 tw-w-[217px]">Learn more</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 col-lg-5 px-0 order-0 order-md-1">
+                    <div className="background-image-cover tw-min-h-[calc(100vh-79px)]" style={{backgroundImage: `url(${marso})`}}></div>
+                </div>
+            </div>
+
+            <div className="bg-color-2 py-5">
+                <div className="container py-5">
+                    <p className="text-center font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700 text-white mb-5">The Process</p>
+
+                    <div className="row">
+                        <div className="col-md-4 md:tw-mt-[50px] mb-5 mb-md-0">
+                            <div className="text-center mb-4">
+                                <div className="d-inline-block">
+                                    <div className="tw-bg-[#F7CE00] rounded-circle d-flex justify-content-center align-items-center tw-w-[90px] tw-h-[90px]">
+                                        <div className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700 tw-text-[#161616]">1</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p className="text-center text-white font-size-110 px-lg-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+
+                        <div className="col-md-4 md:tw-mt-[120px] mb-5 mb-md-0">
+                            <div className="text-center mb-4">
+                                <div className="d-inline-block">
+                                    <div className="tw-bg-[#D7EDBD] rounded-circle d-flex justify-content-center align-items-center tw-w-[90px] tw-h-[90px]">
+                                        <div className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700 tw-text-[#161616]">2</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p className="text-center text-white font-size-110 px-lg-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="text-center mb-4">
+                                <div className="d-inline-block">
+                                    <div className="tw-bg-[#23D8D4] rounded-circle d-flex justify-content-center align-items-center tw-w-[90px] tw-h-[90px]">
+                                        <div className="font-size-160 font-size-lg-230 font-size-xl-260 font-weight-700 tw-text-[#161616]">3</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <p className="text-center text-white font-size-110 px-lg-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                     </div>
                 </div>
